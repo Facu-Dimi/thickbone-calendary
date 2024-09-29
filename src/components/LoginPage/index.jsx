51e118d8f4a83e7f248b661c7805033060dbf1f7
Facu-Dimi <dimitrioff.facundo.et21.21@gmail.com>
2024-09-29 03:13:40 -0300

    Login Page

---
+++ src/components/LoginPage/index.jsx
@@ -0,0 +1,22 @@
+import "./loginpage.css"
+import React from "react";
+
+
+export function LoginPage (){
+    return(
+        <div class="logos-login">
+            <div class="logos">
+            <img class="logo" src="/img/logo.png" alt="thickbone-logo"/>
+            <img class="logo-name" src="/img/logo-name.webp" alt="logo-name"/>
+            <img class="calendar" src="/img/calendar.png" alt="calendar"/>
+            </div>
+            <img  class="race" src="/img/race.jpg" alt="race"/>
+
+            <div class="login">
+                <div>
+                    <img class="fondo" src="/img/fondo-login.jpg" alt="fondo" />
+                </div>
+            </div>
+        </div>
+    )
+}
